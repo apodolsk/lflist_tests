aba71a8b2df9a8e9e28b82506b1e2ef144189985
Alex V. Podolsky <apodolsk@andrew.cmu.edu>
2014-04-16 00:37:32 -0400

    More portable whtypes.h

--- src/whtypes.h
+++ src/whtypes.h
@@ -9,7 +9,6 @@ typedef unsigned int uint;
 typedef uintptr_t uptr;
 typedef uptr cnt;
 typedef intptr_t iptr;
-typedef __int128_t dptr;
 typedef size_t size;
 typedef ptrdiff_t ptrdiff;
 typedef int err;
@@ -18,13 +17,17 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef __uint128_t u128;
 
 typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
-typedef __int128_t i128;
+
+#if _LP64
+typedef __int128_t dptr;
+#else
+typedef int64_t dptr;
+#endif
 
 #define WORDBITS (8 * sizeof(void *))
 
