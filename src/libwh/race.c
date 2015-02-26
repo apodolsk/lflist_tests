307e30f53ec51898bddbcccef3f0dde536f47eef
Alex V. Podolsky <apodolsk@andrew.cmu.edu>
2015-02-26 00:52:54 -0500

    Forgot to add race.c.

---
+++ src/libwh/race.c
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <wrand.h>
+#include <tid.h>
+
+extern err kyield(tid t);
+void race(cnt maxns, uint pcnt, uint idmod){
+    if(!interrupts_enabled())
+        return;
+    if(randpcnt(pcnt >> 1))
+        (kyield)((dptr) -1);
+    else if(0 == mod_pow2(PUN(uptr, get_dbg_id()), idmod) && randpcnt(pcnt))
+        nanosleep(&(struct timespec){.tv_nsec = wrand() % maxns}, NULL);
+}
