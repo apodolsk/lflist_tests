99980d0df7c90ee4f808b9ee0438ecb9e9f510ac
Alex V. Podolsky <podolsky.sasha@gmail.com>
2016-09-27 17:09:01 -0400

    Passing all but -p4. Seeing the same old segfault when writing to a freshly mmapped slab in nalloc.

--- src/errverbs.h
+++ src/errverbs.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#define DBG 1
+#define DBG 0
 #define BRK LVL_EWTF
 #define PRNT LVL_EOOR
 
-#define E_MODULE DBG BRK PRNT
+#define E_MODULE DBG, BRK, PRNT
+#define E_ATOMICS 0, BRK, PRNT
 
